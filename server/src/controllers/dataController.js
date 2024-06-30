40e138562c4b892f1273f7c4501b341b83fbf508
ayushkr012 <bt21cse014@iiitn.ac.in>
2024-06-30 18:45:12 +0530

    file updated

--- server/src/controllers/dataController.js
+++ server/src/controllers/dataController.js
@@ -12,12 +12,12 @@ exports.getData = async (req, res) => {
 
 exports.addData = async (req, res) => {
   try {
-    // const newData = new Data(req.body);
-    // const savedData = await newData.save();
-    const jsonData = req.body; // Assumes the JSON data is sent in the body of the POST request
-    await Data.insertMany(jsonData);
-    res.status(200).send("Data inserted successfully");
-    // res.status(201).json(savedData);
+    const newData = new Data(req.body);
+    const savedData = await newData.save();
+    // const jsonData = req.body; // for inserting multiple data at once
+    // await Data.insertMany(jsonData);
+    // res.status(200).send("Data inserted successfully");
+    res.status(201).json(savedData);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server error" });
